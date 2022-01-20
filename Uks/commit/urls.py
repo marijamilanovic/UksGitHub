c9e6d4cf25c8e236fbdaee98a34149ece5e5eba9
Maja18 <majadragojlovic98@gmail.com>
2022-01-20 20:27:30 +0100

    [FeatureUpdate] url changed

--- Uks/commit/urls.py
+++ Uks/commit/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 from . import views
 
-#app_name = "commit"
+app_name = "commit"
 
 urlpatterns = [
     path('createCommit', views.createCommit, name='newCommit'),
